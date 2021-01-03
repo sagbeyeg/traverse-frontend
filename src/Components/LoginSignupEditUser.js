6046cb0f9035a3ddce1a8024c953966f33c4ff7f
sagbeyeg <agbeyegbesisan@gmail.com>
2021-01-03 03:33:07 -0500

    setup state and change handler for login form

--- src/Components/LoginSignupEditUser.js
+++ src/Components/LoginSignupEditUser.js
@@ -3,31 +3,38 @@ import {Link, NavLink } from 'react-router-dom'
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 class LoginSignupEditUser extends Component {
+  state = {
+    username: '',
+    password: ''
+  }
+
+  changeHandler = (e) => { 
+    this.setState({
+      ...this.state, [e.target.name]: e.target.value
+    }, () => console.log(this.state))
+  }
+
   render() {
     return (
       <>
       <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-        <Grid.Column style={{ maxWidth: 800}} className="card">
-        {/* <Image src='/traverse-logo-white.png' height="150px"/> */}
-          <Header as='h2' color='orange' textAlign='center'>Log in to Traverse</Header>
-          <Form size='large'>
-            <Segment stacked>
-              <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-              <Form.Input
-                fluid
-                icon='lock'
-                iconPosition='left'
-                placeholder='Password'
-                type='password'
-              />
-
-              <Button color='orange' fluid size='large' as={NavLink} to='/locations'>
-                Login
-              </Button>
-            </Segment>
-          </Form>
-          <br></br>
-          <small><h5><Link to="/forgotpassword">Forgot your password?</Link> | <Link to="/signup">Sign up for Exhale</Link></h5></small>
+        <Grid.Column style={{ maxWidth: 800}} >
+            <div class="card">
+            <h1 class="blue">Log in to Traverse</h1> 
+            <form>
+              <div class="form-group">
+                <label for="location">Username</label>
+                <input class="form-control" type="text" name="username" value={this.state.username} onChange={this.changeHandler} ></input>
+              </div>
+              <div class="form-group">
+                <label for="location">Password</label>
+                <input class="form-control" type="password" name="password" value={this.state.password} onChange={this.changeHandler} ></input>
+              </div>
+              <Button primary onClick={this.submitHandler} as={NavLink} to='/profile' >Login</Button>
+            </form>
+              <br></br>
+              <small><h5><Link to="/forgotpassword">Forgot your password?</Link> | <Link to="/signup">Sign up for Exhale</Link></h5></small>
+          </div>
         </Grid.Column>
       </Grid>
       </>
