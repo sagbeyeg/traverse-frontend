edbe030f312cd5f7f18300e47d6bae8bb772a30c
sagbeyeg <agbeyegbesisan@gmail.com>
2021-01-11 20:37:16 -0500

    removed filter header

--- src/Components/ReviewFilter.js
+++ src/Components/ReviewFilter.js
@@ -4,8 +4,6 @@ class ReviewFilter extends Component {
   render() {
     return (
       <div className="filter">
-        <h1>Looking For A Specific Destination?</h1>
-     
         <select name="type" id="type" onChange={this.props.onChangeType}>
           <option name="all">All Reviews</option>
           <option name="myReviews">My Reviews</option>
